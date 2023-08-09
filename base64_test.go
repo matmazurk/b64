580051b8873f76d3958944db205701bdeb1c8b87
Mateusz Mazurkiewicz <mateusz.mazurkiewicz@karhoo.com>
2023-08-09 20:43:09 +0200

    add first example to test

--- base64_test.go
+++ base64_test.go
@@ -12,7 +12,13 @@ func TestEncode(t *testing.T) {
 		name     string
 		in       string
 		expected string
-	}{}
+	}{
+		{
+			name:     "should correctly encode first example",
+			in:       "Many hands make light work.",
+			expected: "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu",
+		},
+	}
 
 	for _, tc := range tcs {
 		t.Run(tc.name, func(t *testing.T) {
