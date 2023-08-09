76e89328e4421c68a3af0af1420468ee9a961745
Mateusz Mazurkiewicz <mateusz.mazurkiewicz@karhoo.com>
2023-08-09 20:41:22 +0200

    Add files structure with test

---
+++ base64_test.go
@@ -0,0 +1,24 @@
+package b64_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestEncode(t *testing.T) {
+	tcs := []struct {
+		name     string
+		in       string
+		expected string
+	}{}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			res, err := b64.Encode(tc.in)
+			require.NoError(t, err)
+
+			require.Equal(t, tc.expected, res)
+		})
+	}
+}
