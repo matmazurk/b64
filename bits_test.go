846ed555246016d3b6dc50834832699d0d93cf84
Mateusz Mazurkiewicz <mateusz.mazurkiewicz@karhoo.com>
2023-08-15 08:51:41 +0200

    add bits tests

---
+++ bits_test.go
@@ -0,0 +1,135 @@
+package b64
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_bits_addLeft(t *testing.T) {
+	tests := []struct {
+		input    *bits
+		toAdd    *bits
+		expected *bits
+	}{
+		{
+			input:    newBits(0, 0),
+			toAdd:    newBits(0, 0),
+			expected: newBits(0, 0),
+		},
+		{
+			input:    newBits(5, 0b10000),
+			toAdd:    newBits(0, 0),
+			expected: newBits(5, 0b10000),
+		},
+		{
+			input:    newBits(0, 0),
+			toAdd:    newBits(5, 0b10000),
+			expected: newBits(5, 0b10000),
+		},
+		{
+			input:    newBits(5, 0b11111),
+			toAdd:    newBits(5, 0b10000),
+			expected: newBits(10, 0b1000011111),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(
+			fmt.Sprintf("%s.addLeft(%s) should result in %s", tc.input, tc.toAdd, tc.expected),
+			func(t *testing.T) {
+				in := tc.input
+				in.addLeft(tc.toAdd)
+
+				if !in.equals(tc.expected) {
+					t.Errorf("actual different than expected: \n%s != \n%s", in, tc.expected)
+				}
+			},
+		)
+	}
+}
+
+func Test_bits_addRight(t *testing.T) {
+	tests := []struct {
+		input    *bits
+		toAdd    *bits
+		expected *bits
+	}{
+		{
+			input:    newBits(0, 0),
+			toAdd:    newBits(0, 0),
+			expected: newBits(0, 0),
+		},
+		{
+			input:    newBits(5, 0b10000),
+			toAdd:    newBits(0, 0),
+			expected: newBits(5, 0b10000),
+		},
+		{
+			input:    newBits(0, 0),
+			toAdd:    newBits(5, 0b10000),
+			expected: newBits(5, 0b10000),
+		},
+		{
+			input:    newBits(5, 0b11111),
+			toAdd:    newBits(5, 0b10000),
+			expected: newBits(10, 0b1111110000),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(
+			fmt.Sprintf("%s.addRight(%s) should result in %s", tc.input, tc.toAdd, tc.expected),
+			func(t *testing.T) {
+				in := tc.input
+				in.addRight(tc.toAdd)
+
+				if !in.equals(tc.expected) {
+					t.Errorf("actual different than expected: \n%s != \n%s", in, tc.expected)
+				}
+			},
+		)
+	}
+}
+
+func Test_bits_cutSignificantBits(t *testing.T) {
+	tests := []struct {
+		in          *bits
+		toCut       int
+		expectedCut *bits
+		expectedIn  *bits
+	}{
+		{
+			in:          newBits(0, 0),
+			toCut:       0,
+			expectedCut: newBits(0, 0),
+			expectedIn:  newBits(0, 0),
+		},
+		{
+			in:          newBits(6, 0b110011),
+			toCut:       3,
+			expectedCut: newBits(3, 0b110),
+			expectedIn:  newBits(3, 0b011),
+		},
+		{
+			in:          newBits(8, 0b00110011),
+			toCut:       5,
+			expectedCut: newBits(5, 0b00110),
+			expectedIn:  newBits(3, 0b011),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(
+			fmt.Sprintf("%s.cutSignificantBits(%d) should result in %s", tc.in, tc.toCut, tc.expectedCut),
+			func(t *testing.T) {
+				in := tc.in
+				cut := in.cutSignificantBits(tc.toCut)
+
+				if !cut.equals(tc.expectedCut) {
+					t.Errorf("actual cut different than expected: \n%s != \n%s", cut, tc.expectedCut)
+				}
+
+				if !in.equals(tc.expectedIn) {
+					t.Errorf("actual in different than expected: \n%s != \n%s", in, tc.expectedIn)
+				}
+			},
+		)
+	}
+}
