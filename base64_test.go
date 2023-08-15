121925abbf88adeae5dbcd1fce8fce43740e53c8
Mateusz Mazurkiewicz <mateusz.mazurkiewicz@karhoo.com>
2023-08-15 09:22:43 +0200

    improve tests, add Fuzz tests

--- base64_test.go
+++ base64_test.go
@@ -1,8 +1,11 @@
 package b64_test
 
 import (
+	"bytes"
+	"fmt"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/matmazurk/b64"
 	"github.com/stretchr/testify/require"
 )
@@ -10,36 +13,60 @@ import (
 func TestEncodeAndDecode(t *testing.T) {
 	emptyBytes := []byte{}
 	tcs := []struct {
-		in       []byte
-		expected []byte
+		in  []byte
+		out []byte
 	}{
 		{
-			in:       emptyBytes,
-			expected: emptyBytes,
+			in:  emptyBytes,
+			out: emptyBytes,
 		},
 		{
-			in:       []byte("Many hands make light work."),
-			expected: []byte("TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu"),
+			in:  []byte("Many hands make light work."),
+			out: []byte("TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu"),
 		},
 		{
-			in:       []byte("M"),
-			expected: []byte("TQ=="),
+			in:  []byte("M"),
+			out: []byte("TQ=="),
 		},
 		{
-			in:       []byte("Ma"),
-			expected: []byte("TWE="),
+			in:  []byte("Ma"),
+			out: []byte("TWE="),
 		},
 	}
 
 	for _, tc := range tcs {
-		res, err := b64.Encode(tc.in)
-		require.NoError(t, err)
-		require.Equal(t, tc.expected, res)
+		t.Run(
+			fmt.Sprintf("%s encoding should result in %s", string(tc.in), string(tc.out)),
+			func(t *testing.T) {
+				res, err := b64.Encode(tc.in)
+				require.NoError(t, err)
+				require.Equal(t, tc.out, res)
+			},
+		)
 	}
 
 	for _, tc := range tcs {
-		res, err := b64.Decode(tc.expected)
-		require.NoError(t, err)
-		require.Equal(t, res, tc.in)
+		t.Run(
+			fmt.Sprintf("%s decoding should result in %s", string(tc.out), string(tc.in)),
+			func(t *testing.T) {
+				res, err := b64.Decode(tc.out)
+				require.NoError(t, err)
+				require.Equal(t, tc.in, res)
+			},
+		)
+
 	}
+}
+
+func FuzzEncodeAndDecode(f *testing.F) {
+	f.Fuzz(func(t *testing.T, bt []byte) {
+		enc, err := b64.Encode(bt)
+		require.NoError(t, err)
+
+		dec, err := b64.Decode(enc)
+		require.NoError(t, err)
+		if !bytes.Equal(bt, dec) {
+			t.Errorf("decoded different than original input: %s", cmp.Diff(bt, dec))
+		}
+	})
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/matmazurk/b64
 
 go 1.21
 
-require github.com/stretchr/testify v1.8.4
+require (
+	github.com/google/go-cmp v0.5.9
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
